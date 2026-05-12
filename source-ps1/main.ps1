f49b4901baa049458893fb61f8ac797ef76d3363
Sargon1729 <shadow-elephant@proton.me>
2026-05-12 12:45:33 +0100

    removed config.ps1

--- source-ps1/main.ps1
+++ source-ps1/main.ps1
@@ -5,14 +5,20 @@ write_log "----SCRIPT STARTED----"
 ################################################################################## Verifying config file
 
 if ($USE_RSA -and $USE_EC) {
-    write_error "Cannot enable both RSA and EC at the same time"
+    write_error "Cannot enable both RSA and EC at the same time, exiting."
+    break
 }
 
 if ( -not $USE_RSA -and -not $USE_EC) {
-    write_error "USE_RSA and USE_EC cannot be both set to false"
+    write_error "USE_RSA and USE_EC cannot be both set to false, exiting."
+    break
 }
 
 ################################################################################## Create working folder
 $CN= (Get-Content csr.conf | Select-String "CN" -CaseSensitive).ToString().Split("=")[-1].Trim()
 
-if ()
+if (!(Test-Path ..\certs\$CN)) {
+    write_log "dir $CN does not exist"; write_log "Creating dir $CN"
+    mkdir ..\certs\$CN
+
+}
